8f070550c96daee3bfe784fbd1a5544819fa40c3
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-07-23 22:56:35 -0400

    add more code from ch3

--- ch3/script1.py
+++ ch3/script1.py
@@ -1,9 +1,7 @@
 # A first Python script
 
-import sys
-
-print(sys.platform)
-print(2 ** 100)
-
-x = 'Spam!'
-print(x * 8)
+import sys           # load a module
+print(sys.platform)  # print out information about the system
+print(2 ** 100)      # print 2 to the power of 100
+x = 'Spam!'          # assign the string 'Spam!' to the variable x
+print(x * 8)         # print x 8 times
